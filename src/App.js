7902a61cff1afae5d464e7186d86fa15c8bfeab9
Tijs G <tijs.gommeren@gmail.com>
2022-12-23 14:29:54 +0100

    minimal layout, bootstrap and progressbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import { createWorker } from "tesseract.js";
-import "./App.css";
+import styled from "styled-components";
+import Container from "./components/Container";
+import { Button, ProgressBar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [imagePath, setImagePath] = useState("");
   const [text, setText] = useState("");
+  const [progress, setProgress] = useState(0);
 
   const handleChange = (event) => {
     setImagePath(URL.createObjectURL(event.target.files[0]));
   };
 
+  const clearImage = () => {
+    setImagePath("");
+    setText("");
+    setProgress(0);
+  };
+
   const handleClick = async () => {
     const worker = await createWorker({
-      logger: (m) => console.log(m), // Add logger here
+      logger: (m) => {
+        console.log(m);
+        setProgress(parseInt(m?.progress * 100));
+      }, // Add logger here
     });
 
     (async () => {
@@ -28,22 +41,81 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <main className="App-main">
-        <h3>Actual image uploaded</h3>
-        <img src={imagePath} className="App-logo" alt="logo" />
-        <h3>Extracted text</h3>
-        <div className="pin-box">
-          <p> OCR text: {text} </p>
-        </div>
-        <input type="file" onChange={handleChange} />
-        <button onClick={handleClick} style={{ height: 50 }}>
-          {" "}
-          convert to text
-        </button>
+    <div>
+      <main>
+        <Container>
+          <h1>Offline image OCR</h1>
+          <h2>Upload file</h2>
+          <input type="file" onChange={handleChange} />
+          {imagePath && (
+            <>
+              <Steps>
+                <ImageWrapper>
+                  <>
+                    <h3>Actual image uploaded</h3>
+                    <UploadedImage src={imagePath} />
+                    <Button
+                      variant={"warning"}
+                      onClick={clearImage}
+                      style={{ height: 50 }}
+                    >
+                      clear
+                    </Button>
+                  </>
+                </ImageWrapper>
+                <CTAWrapper>
+                  <Button onClick={handleClick} style={{ height: 50 }}>
+                    convert to text
+                  </Button>
+                </CTAWrapper>
+                <OutputWrapper>
+                  <>
+                    <h3>Extracted text</h3>
+                    <p> {text}</p>
+                  </>
+                </OutputWrapper>
+              </Steps>
+              <ProgressBarWrapper>
+                {progress === (0 || 100) ? (
+                  <ProgressBar now={progress} label={`${progress}%`} />
+                ) : (
+                  <ProgressBar animated now={progress} label={`${progress}%`} />
+                )}
+              </ProgressBarWrapper>
+            </>
+          )}
+        </Container>
       </main>
     </div>
   );
 }
 
 export default App;
+
+const Steps = styled.div`
+  display: flex;
+  gap: 20px;
+  margin: 100px 0;
+`;
+
+const UploadedImage = styled.img`
+  max-width: 300px;
+`;
+
+const CTAWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const OutputWrapper = styled.div`
+  max-width: 300px;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProgressBarWrapper = styled.div`
+  margin: 50px 0;
+`;
